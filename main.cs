e76eb8b5503d4e58e19a0ec172edb01cd1cf3ba1
heofen <heofen@heofen.cum>
2024-11-05 16:44:50 +0300

    Exam

--- main.cs
+++ main.cs
@@ -61,3 +61,50 @@ public class Person
         return $"{firstName} {lastName}"
     }
 }
+
+public class Exam
+{
+    public string Subject {get; set;}
+    public int Grade {get; set;}
+    public DateTime Date {get; set;}
+
+    public Exam(string subject, int grade, DateTime date)
+    {
+        Subject = subject;
+        Grade = grade;
+        Date = date;
+    }
+
+    public override string ToString()
+    {
+        return $"Subject: {Subject}, Grade: {Grade}, Date: {Date..ToShortDateString()}"
+    }
+}
+
+public class Student
+{
+    private Person person;
+    private Education education;
+    private int groupNumber;
+    private List<Exam> exams;
+
+    public Student(Person person, Education education, int groupNumber)
+    {
+        this.person = person;
+        this.education = education;
+        this.groupNumber = groupNumber;
+        this.exams = new List<Exam>();
+    }
+
+    public Person PersonInfo
+    {
+        get {return person;}
+        set {person = value;}
+    }
+
+    public Education EducationForm
+    {
+        get {return education;}
+        set {education = value;}
+    }
+}
